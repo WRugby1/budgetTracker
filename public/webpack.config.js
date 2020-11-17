ed2d29f1c3290c2cc1f4705401e10d1eb8b001e8
Tanner Austin <tanner.a.austin@gmail.com>
2020-11-17 16:01:55 -0600

    Functionality

---
+++ public/webpack.config.js
@@ -0,0 +1,48 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: '/index.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget tracker app',
+      short_name: 'Budget',
+      description: 'An application that allows you to track your budget online or off.',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('/icons/icon-512x512.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('public', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
